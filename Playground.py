1c0a0124c68eb56bb93a98d0bfc31163526cec5e
tim <tim@192.168.1.149>
2018-01-07 21:40:48 +0000

    TestCommit
    Change-Id: Ic51947c665b0832e830aa9e6ca5e49bd5c6ce2c4

--- Playground.py
+++ Playground.py
@@ -1,1 +1,11 @@
 # test things here
+
+# Idea - try to create data at a given interval as part of the python code, In parallel write this to a mysql database.
+
+import pandas
+
+datapath = "/Users/tim/git/MachineLearning/occupancy_data/datatest.txt"
+
+dataset = pandas.read_csv(datapath)
+print(dataset.shape)
+print(dataset.head(20))
